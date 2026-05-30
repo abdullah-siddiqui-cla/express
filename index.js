8a1cf94e7b2c706f65fc1174292629b78c1cd20e
Abdullah <abdullah.siddiqui@codelabsacademy.com>
2026-05-30 11:52:41 +0500

    Added the npm start

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ app.use((req, res, next) => {
   });
 });
 
-app.listen(3011, () => {
-  console.log('Server is running on port 3011');
+const PORT = process.env.PORT || 3011;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
